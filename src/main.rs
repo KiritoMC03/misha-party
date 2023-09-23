972bb16f14430a0e352b8d3b1ad0036ab8482658
Dudnik Kirill <prog.me.2017@gmail.com>
2023-09-23 14:11:14 +0200

    - added actix-web and main_page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    App,
+    HttpResponse,
+    HttpServer,
+    Responder
+};
+
+use actix_web:: {
+    get,
+};
+
+const IP : &str = "0.0.0.0";
+const PORT : u32 = 5000;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let app = App::new()
+            .service(main_page);
+    HttpServer::new(|| {app})
+        .bind((IP, PORT))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn main_page() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }

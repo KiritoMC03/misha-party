1fc30626d6099985cf721b9f4566b4bb91a88268
Dudnik Kirill <prog.me.2017@gmail.com>
2023-09-24 13:42:03 +0200

    - added test chat app

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,47 @@
 #[macro_use] extern crate rocket;
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![default_page, hello])
+use rocket::{State, Shutdown};
+use rocket::form::Form;
+use rocket::fs::{FileServer, relative};
+use rocket::response::stream::{EventStream, Event};
+use rocket::serde::{Serialize, Deserialize};
+use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::tokio::select;
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
+}
+
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut receiver = queue.subscribe();
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = receiver.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
+}
+
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    // A send 'fails' if there are no active subscribers. That's okay.
+    let _res = queue.send(form.into_inner());
 }
 
 #[get("/")]
@@ -11,7 +49,10 @@ fn default_page() -> &'static str {
     "Misha - pidor!"
 }
 
-#[get("/hello/<name>")]
-fn hello(name: &str) -> String {
-    format!("Hello, {}!", name)
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(channel::<Message>(1024).0)
+        .mount("/", routes![post, events])
+        .mount("/", FileServer::from(relative!("static")))
 }

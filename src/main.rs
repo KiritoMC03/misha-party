6c0341a0c21506839c9f197db797ce4c08dd24b4
Dudnik Kirill <prog.me.2017@gmail.com>
2023-09-24 12:47:42 +0200

    - added rocket framework

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
-use actix_web::{
-    App,
-    HttpResponse,
-    HttpServer,
-    Responder
-};
+#[macro_use] extern crate rocket;
 
-use actix_web:: {
-    get,
-};
-
-const IP : &str = "0.0.0.0";
-const PORT : &str = "8000";
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let port = std::env::var("PORT")
-        .unwrap_or_else(|_| PORT.to_string())
-        .parse()
-        .expect("PORT must be a number");
-
-    HttpServer::new(move || {
-        App::new()
-            .service(main_page)
-    })
-        .bind((IP, port))?
-        .run()
-        .await
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![default_page])
 }
 
 #[get("/")]
-async fn main_page() -> impl Responder {
-    HttpResponse::Ok().body("Misha - pidor!")
+fn default_page() -> &'static str {
+    "Misha - pidor!"
 }

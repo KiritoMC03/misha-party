43c7c84527bc4a59bf3d12a510860c742d94c5a6
Dudnik Kirill <prog.me.2017@gmail.com>
2023-10-01 16:57:29 +0200

    - changed routing

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use rocket_ws::{Stream, WebSocket};
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![main_page, echo_stream, favicon, echo_stream_s, echo_stream_ss, echo_stream_sss])
+        .mount("/", routes![main_page, favicon])
+        .mount("/ws", routes![echo_stream])
 }
 
 #[get("/")]
@@ -14,45 +15,9 @@ async fn main_page() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/index.html")).await.ok()
 }
 
-#[get("/ws")]
+#[get("/")]
 fn echo_stream(ws: WebSocket) -> Stream!['static] {
     println!("ws enter 1");
-    Stream! { ws =>
-        for await message in ws {
-            let message = message.unwrap();
-            println!("{}", message);
-            yield message;
-        }
-    }
-}
-
-#[get("/ws/ws")]
-fn echo_stream_s(ws: WebSocket) -> Stream!['static] {
-    println!("ws enter 2");
-    Stream! { ws =>
-        for await message in ws {
-            let message = message.unwrap();
-            println!("{}", message);
-            yield message;
-        }
-    }
-}
-
-#[get("/wss")]
-fn echo_stream_ss(ws: WebSocket) -> Stream!['static] {
-    println!("ws enter 3");
-    Stream! { ws =>
-        for await message in ws {
-            let message = message.unwrap();
-            println!("{}", message);
-            yield message;
-        }
-    }
-}
-
-#[get("/wss/ws")]
-fn echo_stream_sss(ws: WebSocket) -> Stream!['static] {
-    println!("ws enter 4");
     Stream! { ws =>
         for await message in ws {
             let message = message.unwrap();

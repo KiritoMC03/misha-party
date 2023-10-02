843871ded73aaee74f88e774cd693c9f7c1272f5
Dudnik Kirill <prog.me.2017@gmail.com>
2023-10-02 21:37:55 +0200

    - added interval messages

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use std::path::Path;
 use rocket::fs::NamedFile;
-use rocket::{catchers, get, launch, routes};
+use rocket::{catchers, get, launch, routes, State};
+use rocket::tokio::sync::Mutex;
 use rocket_ws::{Stream, WebSocket};
 use args::*;
 
@@ -14,6 +15,7 @@ fn rocket() -> _ {
     let args: Vec<String> = env::args().collect();
     let mut server = rocket::build()
         .mount("/", routes![index, favicon, echo_stream])
+        .manage(Sockets{ list: Mutex::new(Vec::new()) })
         .register("/", catchers![catchers::not_found]);
 
     if args.contains(&LOG_REQUESTS.to_string()) {
@@ -32,7 +34,8 @@ async fn index() -> Option<NamedFile> {
 }
 
 #[get("/echo")]
-async fn echo_stream(ws: WebSocket) -> Stream!['static] {
+async fn echo_stream(ws: WebSocket, _sockets: &State<Sockets>) -> Stream!['static] {
+    // sockets.list.lock().await.push(ws);
     Stream! { ws =>
         for await message in ws {
             let message = message.unwrap();
@@ -45,4 +48,8 @@ async fn echo_stream(ws: WebSocket) -> Stream!['static] {
 #[get("/favicon.ico")]
 async fn favicon() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/favicon.ico")).await.ok()
+}
+
+struct Sockets {
+    pub list: Mutex<Vec<WebSocket>>,
 }
